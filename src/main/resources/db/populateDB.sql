89387b10b0edd97374f36b632f23d4688a92129b
Zernov Aleksey <aleksey.zernov@zimad.com>
2025-03-20 22:20:16 +0300

    add datajpa, init db(hsqldb))

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,10 +1,21 @@
-DELETE
-FROM restaurant;
-DELETE
-FROM menuitem;
-DELETE
-FROM menuitem;
+DELETE FROM vote;
+DELETE FROM menu_history;
+DELETE FROM menu_menuitem;
+DELETE FROM menuitem;
+DELETE FROM menu;
+DELETE FROM restaurant;
+DELETE FROM user_role;
+DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
+
+INSERT INTO users (name, email, password)
+VALUES ('User', 'user@yandex.ru', 'password'),
+       ('Admin', 'admin@gmail.com', 'admin'),
+       ('Guest', 'guest@gmail.com', 'guest');
+
+INSERT INTO user_role (role, user_id)
+VALUES ('USER', 100000),
+       ('ADMIN', 100001);
 
 INSERT INTO restaurant (name)
 VALUES ('Restaurant A'),
