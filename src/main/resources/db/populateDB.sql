0705b8f362bf9166a446351b6a832771dbdf0b1b
Zernov Aleksey <aleksey.zernov@zimad.com>
2025-03-24 17:20:20 +0300

    fix hibernate sql query logs config, fix populate db

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -24,22 +24,41 @@ VALUES ('Restaurant A'),
        ('Restaurant B'),
        ('Restaurant C');
 
+INSERT INTO menuitem (name, price)
+VALUES ('Burger', 450),
+       ('Pizza', 800),
+       ('Salad', 250),
+       ('Dessert', 350),
+       ('Soup', 200),
+       ('Eggs', 150),
+       ('Steak', 900),
+       ('Sushi', 750),
+       ('Chicken', 400),
+       ('Coffee', 100);
+
+INSERT INTO menu
+VALUES (DEFAULT),
+       (DEFAULT),
+       (DEFAULT);
+
+INSERT INTO menu_menuitem_link (menu_id, menuitem_id)
+VALUES (100017, 100007),
+       (100017, 100008),
+       (100017, 100009),
+       (100017, 100010),
+       (100018, 100011),
+       (100018, 100012),
+       (100019, 100013),
+       (100019, 100014),
+       (100019, 100010),
+       (100019, 100016),
+       (100019, 100012);
+
 INSERT INTO vote (date_time, user_id, restaurant_id)
-VALUES ('2025-03-20 07:00:00', '100000', '100004'),
+VALUES (CAST(CURRENT_DATE AS TIMESTAMP) + INTERVAL '9' HOUR + INTERVAL '45' MINUTE, '100000', '100006'),
+       ('2025-03-20 07:00:00', '100000', '100004'),
        ('2025-03-20 08:00:00', '100001', '100005'),
        ('2025-03-20 09:00:00', '100002', '100006'),
        ('2025-03-21 08:00:00', '100000', '100005'),
        ('2025-03-21 09:00:00', '100001', '100004'),
-       ('2025-03-21 10:00:00', '100002', '100006');
-
-INSERT INTO menuitem (name, price)
-VALUES ('Burger', 300),
-       ('Pizza', 800),
-       ('Salad', 200),
-       ('Dessert', 450),
-       ('Soup', 150),
-       ('eggs', '300'),
-       ('steak', '400'),
-       ('shi', '500');
-
-
+       ('2025-03-21 09:10:00', '100002', '100006');
