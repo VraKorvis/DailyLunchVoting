0bb62cad90efb3ee16d77b17799db21dea683cc4
Zernov Aleksey <aleksey.zernov@zimad.com>
2025-04-11 20:17:01 +0300

    fix cascade delete roles

--- src/main/java/ru/javapractice/dailylunchvoting/model/User.java
+++ src/main/java/ru/javapractice/dailylunchvoting/model/User.java
@@ -38,6 +38,7 @@ public class User extends AbstractNamedBaseEntity {
             uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "role"}, name = "user_roles_idx")})
     @Column(name = "role")
     @ElementCollection(fetch = FetchType.EAGER)
+    @JoinColumn
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Set<Role> roles;
 
