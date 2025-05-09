d52f932e09ad443e4adf440e0a37dc5d04a3ddee
Zernov Aleksey <aleksey.zernov@zimad.com>
2025-05-10 02:34:38 +0300

    fix tests data

--- src/test/java/ru/javapractice/dailylunchvoting/restaurant/RestaurantMenuData.java
+++ src/test/java/ru/javapractice/dailylunchvoting/restaurant/RestaurantMenuData.java
@@ -2,11 +2,10 @@ package ru.javapractice.dailylunchvoting.restaurant;
 
 import org.assertj.core.api.recursive.comparison.RecursiveComparisonConfiguration;
 import ru.javapractice.dailylunchvoting.restaurant.model.Menu;
-import ru.javapractice.dailylunchvoting.restaurant.model.MenuItem;
+import ru.javapractice.dailylunchvoting.restaurant.model.MenuItemAssignment;
 import ru.javapractice.dailylunchvoting.restaurant.model.Restaurant;
 import ru.javapractice.dailylunchvoting.restaurant.to.MenuTo;
-import ru.javapractice.dailylunchvoting.restaurant.to.RestaurantWithMenuTo;
-import ru.javapractice.dailylunchvoting.user.UserData;
+import ru.javapractice.dailylunchvoting.restaurant.to.RestaurantWithAssignedMenuTo;
 import ru.javapractice.dailylunchvoting.util.MatcherFactory;
 
 import java.math.BigDecimal;
@@ -23,7 +22,7 @@ public class RestaurantMenuData {
             .build();
 
     public static final MatcherFactory.Matcher<Restaurant> MATCHER = MatcherFactory.usingIgnoringFieldsComparator(Restaurant.class, "menus");
-    public static final MatcherFactory.Matcher<RestaurantWithMenuTo> RESTAURANT_TO_MATCHER = MatcherFactory.usingIgnoringFieldsComparator(RestaurantWithMenuTo.class);
+    public static final MatcherFactory.Matcher<RestaurantWithAssignedMenuTo> RESTAURANT_TO_MATCHER = MatcherFactory.usingIgnoringFieldsComparator(RestaurantWithAssignedMenuTo.class);
     public static final MatcherFactory.Matcher<MenuTo> MENU_TO_MATCHER = MatcherFactory.usingIgnoringFieldsComparator(MenuTo.class, buildConfig);
 
     public static final int RESTAURANT_A_ID = START_SEQ + 4;
@@ -32,24 +31,63 @@ public class RestaurantMenuData {
     public static final Restaurant RESTAURANT_B = new Restaurant(RESTAURANT_A_ID + 1, "Restaurant B");
     public static final Restaurant RESTAURANT_C = new Restaurant(RESTAURANT_A_ID + 2, "Restaurant C");
 
-    public static final List<MenuItem> MENUITEMS_1 = List.of(BURGER, PIZZA, SALAD, DESSERT);
-    public static final List<MenuItem> MENUITEMS_2 = List.of(SOUP, EGGS);
-    public static final List<MenuItem> MENUITEMS_3 = List.of(DESSERT, EGGS, STEAK, SUSHI, COFFEE);
+    public static final Menu MENU_1 = new Menu(100017, LocalDate.now(), RESTAURANT_A, null);
+    public static final Menu MENU_2 = new Menu(100018, LocalDate.now(), RESTAURANT_A, null);
+    public static final Menu MENU_3 = new Menu(100019, LocalDate.now(), RESTAURANT_A, null);
 
-    public static final List<MenuItem> NEW_MENUITEMS = List.of(BURGER, COFFEE, SALAD, DESSERT);
-    public static final List<MenuItem> WRONG_NEW_MENUITEMS = List.of(SALMON, GRILLED_CHEESE, new MenuItem(UserData.USER_1_ID, "Coffee", BigDecimal.valueOf(100.0)));
-    public static final List<MenuItem> UPDATED_MENUITEMS_1 = List.of(BURGER, COFFEE, SALAD, DESSERT);
+    public static final Menu NEW_MENU = new Menu(null, LocalDate.now(), RESTAURANT_C, null);
 
-    public static final Menu MENU_1 = new Menu(100017, LocalDate.now(), RESTAURANT_A, MENUITEMS_1);
-    public static final Menu MENU_2 = new Menu(100018, LocalDate.now(), RESTAURANT_A, MENUITEMS_2);
-    public static final Menu MENU_3 = new Menu(100019, LocalDate.now(), RESTAURANT_A, MENUITEMS_3);
+    public static final Menu WRONG_MENU = new Menu(null, LocalDate.now(), RESTAURANT_B, null);
+    public static final Menu UPDATED_MENU = new Menu(100017, LocalDate.now(), RESTAURANT_A, null);
 
-    public static final Menu NEW_MENU = new Menu(null, LocalDate.now(), RESTAURANT_C, NEW_MENUITEMS);
+    public static final List<MenuItemAssignment> MENUITEMS_1 = List.of(
+            new MenuItemAssignment(MENU_1, BURGER, BigDecimal.valueOf(450)),
+            new MenuItemAssignment(MENU_1, PIZZA, BigDecimal.valueOf(800)),
+            new MenuItemAssignment(MENU_1, SALAD, BigDecimal.valueOf(250)),
+            new MenuItemAssignment(MENU_1, DESSERT, BigDecimal.valueOf(350))
+    );
 
-    public static final Menu WRONG_MENU = new Menu(null, LocalDate.now(), RESTAURANT_B, WRONG_NEW_MENUITEMS);
-    public static final Menu UPDATED_MENU = new Menu(100017, LocalDate.now(), RESTAURANT_A, UPDATED_MENUITEMS_1);
+    public static final List<MenuItemAssignment> MENUITEMS_2 = List.of(
+            new MenuItemAssignment(MENU_2, SOUP, BigDecimal.valueOf(200)),
+            new MenuItemAssignment(MENU_2, EGGS, BigDecimal.valueOf(150))
+    );
+
+    public static final List<MenuItemAssignment> MENUITEMS_3 = List.of(
+            new MenuItemAssignment(MENU_3, DESSERT, BigDecimal.valueOf(350)),
+            new MenuItemAssignment(MENU_3, EGGS, BigDecimal.valueOf(150)),
+            new MenuItemAssignment(MENU_3, STEAK, BigDecimal.valueOf(900)),
+            new MenuItemAssignment(MENU_3, SUSHI, BigDecimal.valueOf(750)),
+            new MenuItemAssignment(MENU_3, COFFEE, BigDecimal.valueOf(100))
+    );
+
+    public static final List<MenuItemAssignment> NEW_MENUITEMS = List.of(
+            new MenuItemAssignment(NEW_MENU, BURGER, BigDecimal.valueOf(450)),
+            new MenuItemAssignment(NEW_MENU, COFFEE, BigDecimal.valueOf(100)),
+            new MenuItemAssignment(NEW_MENU, SALAD, BigDecimal.valueOf(250)),
+            new MenuItemAssignment(NEW_MENU, DESSERT, BigDecimal.valueOf(350))
+    );
+
+    public static final List<MenuItemAssignment> WRONG_NEW_MENUITEMS = List.of(
+            new MenuItemAssignment(WRONG_MENU, SALMON, BigDecimal.valueOf(900)),
+            new MenuItemAssignment(WRONG_MENU, GRILLED_CHEESE, BigDecimal.valueOf(800)),
+            new MenuItemAssignment(WRONG_MENU, COFFEE, BigDecimal.valueOf(100))
+    );
+
+    public static final List<MenuItemAssignment> UPDATED_MENUITEMS_1 = List.of(
+            new MenuItemAssignment(UPDATED_MENU, BURGER, BigDecimal.valueOf(500)),
+            new MenuItemAssignment(UPDATED_MENU, COFFEE, BigDecimal.valueOf(150)),
+            new MenuItemAssignment(UPDATED_MENU, SALAD, BigDecimal.valueOf(300)),
+            new MenuItemAssignment(UPDATED_MENU, DESSERT, BigDecimal.valueOf(400))
+    );
 
     static {
+        MENU_1.setMenuItemAssignments(MENUITEMS_1);
+        MENU_2.setMenuItemAssignments(MENUITEMS_2);
+        MENU_3.setMenuItemAssignments(MENUITEMS_3);
+        NEW_MENU.setMenuItemAssignments(NEW_MENUITEMS);
+        WRONG_MENU.setMenuItemAssignments(WRONG_NEW_MENUITEMS);
+        UPDATED_MENU.setMenuItemAssignments(UPDATED_MENUITEMS_1);
+
         RESTAURANT_A.setMenus(List.of(MENU_1));
         RESTAURANT_B.setMenus(List.of(MENU_2));
     }
